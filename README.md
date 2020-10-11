b225dd07151e2afbb4e306818e02a41446a19b19
Gimna Katugampala <gimnakatugampala1@gmail.com>
2020-10-11 14:20:04 +0530

    Initialze

---
+++ README.md
@@ -0,0 +1,1 @@
+Courtesy: Traversy Media
---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Animate scroll
+$('#view-work').on('click',function(){
+    const images = $('#images').position().top;
+
+    $('html,body').animate({
+        scrollTop:images
+    },
+    900)
+})
